29fcbb0fe402aa951f845cadd3642402b1769e76
aravindakshand <aravindakshan.d@thoughtworks.com>
2025-11-10 12:44:55 +0530

    Controller and Service layers has been setup in the Application

---
+++ src/main/java/com/app/todo/services/TaskService.java
@@ -0,0 +1,19 @@
+package com.app.todo.services;
+
+import com.app.todo.models.Task;
+import com.app.todo.repositories.TaskRepository;
+
+import java.util.List;
+
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+}
